ef594f506ce3afb0f24b795fa159ebe6f500fb42
Ali Tootoonchi <alitootoonchi007@gmail.com>
2023-12-02 15:48:05 +0100

    attack mode

--- Assets/script/PlayerCombat.cs
+++ Assets/script/PlayerCombat.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,15 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PlayerAttack();
+        }
         
+        
+    }
+    void PlayerAttack()
+    {
+        animator.SetTrigger("Player Attack");
     }
 }
